e12378a5c8814e34419fef3c763fc5c2150c654c
chessitay <ng842009@big.atid.org.il>
2026-01-18 10:36:47 +0200

    Changed so the simulation to use generated match schedule

--- 2026_fuel_scouting_sim.py
+++ 2026_fuel_scouting_sim.py
@@ -8,108 +8,43 @@ from robot_configs import *
 
 def main():
     MATCHES_PER_ROBOT = 10
-    ITERATIONS = 100000
+    ITERATIONS = 10000
 
     schedule, schedule_score = make_matches(all_robots, MATCHES_PER_ROBOT, ITERATIONS)
-    i=0
-    for match in schedule:
-        print(f"Match {i}: {match.red_alliance} vs {match.blue_alliance}")
-        i+=1
-    print(f"Schedule score: {schedule_score}")
-    return
 
-    robots_to_simulate = ["Quick fire", "Log", "Inconsistent shooting with jam - Askof's function", "Consistent spray fire", "Burst then jam fire", "Stutter wave fire", "Steady fire", "Warmup fire", "Saw fire", "Triangle fire", "Double pulse fire", "Exp decay fire", "Exp ramp fire", "Log ramp fire", "Spike fire", "End push fire", "Jammy fire", "y=6", "67", "t^2"]
+    match_results = []
 
-    # Assigning robots to teams
-    blue_team = []
-    red_team = []
+    scout = MagazineSizeMetric() # The scout creates the data
 
-    random.shuffle(all_robots) # cheep way to make the teams random
+    notification_step = 1  # how often to print progress
+    for i, match in enumerate(schedule): # enumerate takes a list and returns pairs of (index, value)
+        if (i + 1) % notification_step == 0:
+            print("\n" * 2)
+            print("-" * 50)
+            print(f"Match Number: {i + 1}, Blue Team: {match.blue_alliance}, Red Team: {match.red_alliance}")
+            print("-" * 50)
 
-    for i in range(len(all_robots)): # //todo: make it max at 3 robots per team
-        if i % 2 == 0:
-            blue_team.append(all_robots[i])
-        else:
-            red_team.append(all_robots[i])
+        current_match_data = {
+            "match_number": i + 1,
+            "red_team_stats": [],
+            "blue_team_stats": []
+        }
 
-    print("Blue Team:")
-    for robot in blue_team:
-        print(f"- {robot.name}")
+        # Red Team
+        for robot in match.red_alliance:
+            stats = scout_robot_match(robot, scout)
+            current_match_data["red_team_stats"].append(stats)
 
-    print("\nRed Team:")
-    for robot in red_team:
-        print(f"- {robot.name}")
+        # Blue Team
+        for robot in match.blue_alliance:
+            stats = scout_robot_match(robot, scout)
+            current_match_data["blue_team_stats"].append(stats)
 
-    scout = MagazineSizeMetric() # the simulated scouter
+        match_results.append(current_match_data)
 
-    robots_data = [] # a robot spesific data
-
-    for robot in all_robots:
-        if robot.name not in robots_to_simulate: # check if we want to run this robot
-            continue
-
-        robot_stats = scout_robot_match(robot, scout)
-        robots_data.append(robot_stats)
-
-    print(f"\nTeams")
-    print(f"Blue team: {blue_team}")
-    print(f"Red team: {red_team}")
-
-    print("\n" * 2)
-    print("=" * 40)
-    print("FINAL COMPARISON OF ALL THE ROBOTS")
-    print("=" * 40)
-
-    for data in robots_data:
-        print(f"\nRobot Name: {data['name']}")
-        print(f"magazine size: {data['magazine_size']}")
-        print(f"Simulation runs: {data['volleys']}")
-        print(f"- Overall accuracy: {data['accuracy']:.2f}% (placed: {data['placed_accuracy']:.1f}%)")
-        print(f"- Average shots error: {data['shots_error']:.2f}% (real: {data['total_shots']}, scouted: {data['total_scouted_shots']:.1f})")
-        print(f"- Average hits error: {data['hits_error']:.2f}% (real: {data['total_hits']}, scouted: {data['total_scouted_shots']:.1f})")
-
-    print("\n" * 2)
-    print("=" * 16)
-    print("TEAM STATISTICS")
-    print("=" * 16)
-
-    # blue team stats
-    blue_total_shots = 0
-    blue_total_hits = 0
-    blue_total_scouted = 0
-
-    for robot in blue_team: # add the stats to the team stats
-        for data in robots_data:
-            if data["name"] == robot.name:
-                blue_total_shots += data["total_shots"]
-                blue_total_hits += data["total_hits"]
-                blue_total_scouted += data["total_scouted_shots"]
-
-    print("\nBlue Team Stats:")
-    print(f"Robots in team: {[r.name for r in blue_team]}")
-    print(f"Total Shots: {blue_total_shots}")
-    print(f"Total Hits: {blue_total_hits}")
-    print(f"Total Scouted Shots: {blue_total_scouted:.2f}")
-
-    # red team stats
-    red_total_shots = 0
-    red_total_hits = 0
-    red_total_scouted = 0
-
-    for robot in red_team: # add the stats to the team stats
-        for data in robots_data:
-            if data["name"] == robot.name:
-                red_total_shots += data["total_shots"]
-                red_total_hits += data["total_hits"]
-                red_total_scouted += data["total_scouted_shots"]
-
-    print("\nRed Team Results:")
-    print(f"Members: {[r.name for r in red_team]}")
-    print(f"Total Shots: {red_total_shots}")
-    print(f"Total Hits: {red_total_hits}")
-    print(f"Total Scouted Shots: {red_total_scouted:.2f}")
-    print(f"\n") # annoyed me that the terminal line was next to it
-
+    print(f"Schedule Score: {schedule_score}")
+    print("Simulation completed!")
+    print(f"Total matches simulated: {len(match_results)}")
 
 if __name__ == "__main__":
     main()
